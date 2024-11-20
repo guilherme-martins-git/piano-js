ba92a9657979b48b31453b495f705a32d19caedd
guilherme-martins <guilhermesky123@gmail.com>
2024-11-20 20:29:43 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const keys = document.querySelectorAll('.key');
+
+const playNote = (note) => {
+const audio = new Audio(`../notes/${note}.wav`);
+audio.play();
+}
+
+const handleMouseDown = (key) => {
+    playNote(key.getAttribute('data-note'));
+
+ if (key.className.includes('black')) {
+    key.classList.add('black--pressed');
+    return;
+ }
+
+    key.style.background = '#ddd';   
+}
+
+const handleMouseup = (key) => {
+  
+    if (key.className.includes('black')) {
+        key.classList.remove('black--pressed');
+        return;
+    }
+
+    key.style.background = 'white';   
+   }
+
+keys.forEach((key) => {
+
+    key.addEventListener('mousedown', () => handleMouseDown(key))
+    
+    key.addEventListener('mouseup', () => handleMouseup(key))
+});
+
